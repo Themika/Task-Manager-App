b17fb0c1b5ed8e7865adf3b222c6093739be6dfe
Themiika <126718397+Themiika@users.noreply.github.com>
2024-03-17 22:53:31 -0400

    Added better settings layout

--- frontend/src/pages/settings.js
+++ frontend/src/pages/settings.js
@@ -16,61 +16,68 @@ const Settings = () => {
     setPushNotifications(!pushNotifications);
   };
   return (
-    <div className="home">
-      <h1 className="task-details">Settings</h1>
+    <div className="home-settings">
+      <h1 className="task-details-name">Settings</h1>
 
-      <section className="task-details">
-        <h2>Profile</h2>
-        <form>
-          <label>
-            Username:
-            <input type="text" name="username" />
-          </label>
-          <label>
-            Email:
-            <input type="email" name="email" />
-          </label>
-          <button type="submit">Save</button>
-        </form>
-      </section>
+      <div className="settings-container">
+        <div className="left-section">
+          <section className="task-details">
+            <h2>Appearance</h2>
+            <label>
+              Theme:
+              {/* <select onChange={changeTheme}>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+              </select> */}
+            </label>
+          </section>
 
-      <section className="task-details">
-        <h2>Appearance</h2>
-        <label>
-          Theme:
-          {/* <select onChange={changeTheme}>
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-          </select> */}
-        </label>
-      </section>
+          {/* New section for displaying due dates */}
+          <section className="task-details">
+            <h2>Due Dates</h2>
+          </section>
+        </div>
 
-      <section className="task-details">
-        <h2>Notifications</h2>
-        <label>
-          Email Notifications:
-          <FontAwesomeIcon
-            style={{ marginLeft: "50px" }}
-            icon={emailNotifications ? filledSquare : outlinedSquare}
-            color={emailNotifications ? "green" : "grey"}
-            onClick={toggleEmailNotifications}
-          />
-        </label>
-        <label>
-          Push Notifications:
-          <FontAwesomeIcon
-            style={{ marginLeft: "55px" }}
-            icon={pushNotifications ? filledSquare : outlinedSquare}
-            color={pushNotifications ? "green" : "grey"}
-            onClick={togglePushNotifications}
-          />
-        </label>
-      </section>
-
-      {/* New section for displaying due dates */}
-      <section className="task-details">
-        <h2>Due Dates</h2>
-      </section>
+        <div className="right-section">
+          <section className="task-details">
+            <h2>Profile</h2>
+            <form className="form-username">
+              <label className="Username-Input-Form">
+                Username:
+                <input type="text" name="username" />
+              </label>
+              <label>
+                Email:
+                <input type="email" name="email" />
+              </label>
+              <button type="submit">Save</button>
+            </form>
+          </section>
+          <section className="task-details">
+            <h2 className="notification-options-h2">Notification options</h2>
+            <div className="notifications">
+              <label>
+                Email Notifications:
+                <FontAwesomeIcon
+                  style={{ marginLeft: "50px" }}
+                  icon={emailNotifications ? filledSquare : outlinedSquare}
+                  color={emailNotifications ? "green" : "grey"}
+                  onClick={toggleEmailNotifications}
+                />
+              </label>
+              <label>
+                Push Notifications:
+                <FontAwesomeIcon
+                  style={{ marginLeft: "55px" }}
+                  icon={pushNotifications ? filledSquare : outlinedSquare}
+                  color={pushNotifications ? "green" : "grey"}
+                  onClick={togglePushNotifications}
+                />
+              </label>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
